9b44f62a3aaac9ba8b596884643f7e34e92a89c5
Phinocio <github@phinocio.com>
2021-03-19 23:09:22 -0600

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Phinocio</title>
-    </head>
-    <body class="antialiased">
-        
-    </body>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Phinocio</title>
+
+	<link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+	<script>
+		0
+	</script>
+</head>
+
+<body class="antialiased bg-gray-700">
+	<main class="flex min-h-screen justify-center items-center">
+		<p class="text-4xl text-green-500">
+			phinocio.com under construction
+		</p>
+	</main>
+</body>
+
 </html>
