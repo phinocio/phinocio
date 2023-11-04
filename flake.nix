a74c9f91e326789bf36c995a70d05e05174e7339
Phinocio <github@phinocio.com>
2023-11-03 18:17:26 -0600

    add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Add php to workspace so pint and whatnot work";
+
+  # Flake inputs
+  inputs = {
+	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+	let
+		system = "x86_64-linux";
+		pkgs = nixpkgs.legacyPackages.${system};
+	in
+	{
+	  # Development environment output
+	  devShells.x86_64-linux.default = pkgs.mkShell {
+		  packages = with pkgs; [
+			php82
+		  ];
+		};
+	};
+}
