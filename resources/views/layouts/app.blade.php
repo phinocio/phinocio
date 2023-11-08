87a10470139ec0f37cf45e221da857feb1381ad5
Phinocio <github@phinocio.com>
2023-11-08 14:57:39 -0700

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,32 +14,14 @@
 	</head>
 
 	<body class="bg-dark">
-		<nav class="text-black mb-4 justify-between bg-blue-300 text-xl sm:hidden md:flex">
-			<div class="container mx-auto px-4 py-2 sm:flex sm:justify-between md:px-40">
-				<div class="flex items-center justify-between space-x-2">
-					<a href="/" class="mr-0 text-2xl font-extrabold text-red-600 sm:mr-6">Phinocio</a>
-					<a class="hidden rounded px-2 py-1 hover:bg-red-300 sm:block" href="/">Projects</a>
-					<a class="hidden rounded px-2 py-1 hover:bg-red-300 sm:block" href="/thoughts">Random Thoughts</a>
-					<button type="button" class="sm:hidden" onclick="toggleHidden('mobile-menu')">
-						<x-icons.menu />
-					</button>
+		<header class="mb-6 bg-blue-300 px-4 py-2">
+			<nav class="container mx-auto flex flex-col bg-red-200">
+				<div class="flex justify-between align-middle">
+					<a href="/" class="text-xl font-bold">Phinocio</a>
+					<div>right</div>
 				</div>
-
-				<div class="hidden py-4 sm:flex sm:items-center sm:justify-between" id="mobile-menu">
-					<div class="flex flex-col items-center text-center sm:ml-6 sm:flex-row md:space-x-4">
-						<a class="rounded px-2 py-1 hover:bg-red-500 sm:hidden" href="/projects">Projects</a>
-						<a class="rounded px-2 py-1 hover:bg-red-500 sm:hidden" href="/thoughts">Random Thoughts</a>
-						<a href="https://discord.gg/K3KnEgrQE4" target="_blank" rel="noopener noreferrer">
-							<x-icons.discord />
-						</a>
-						<a href="https://github.com/phinocio" target="_blank" rel="noopener noreferrer">
-							<x-icons.github />
-						</a>
-					</div>
-				</div>
-			</div>
-		</nav>
-
+			</nav>
+		</header>
 		<main class="container mx-auto px-4 text-white md:text-lg lg:px-40">{{ $slot }}</main>
 		@vite('resources/js/app.js')
 	</body>
