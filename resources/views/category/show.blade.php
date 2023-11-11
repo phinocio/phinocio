a084cac9b06a160a982c139fbde6a27ba027629b
Phinocio <github@phinocio.com>
2023-11-10 22:53:09 -0700

    refactor: change font size and add title

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,5 +1,7 @@
+@section('title', ucfirst($category->name) . " Thoughts")
+
 <x-app-layout>
-    <h1 class="mb-8 text-2xl font-bold">
+    <h1 class="mb-8 text-3xl font-bold">
         Random Thoughts in <span class="text-blue-500 dark:text-blue-400">{{ $category->name }}</span>
     </h1>
     <x-posts-list :posts="$category->posts" />
